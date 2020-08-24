d6d5ac6d66d8c457f4e4ffbbd383cc95c9e92c8a
sushilkv2004 <sushil_kv2004@yahoo.com>
2020-08-24 06:46:54 -0400

    adding optmizer

--- run_strategy.py
+++ run_strategy.py
@@ -7,7 +7,9 @@ from strategies.BuyHold import BuyHold
 
 cerebro = bt.Cerebro()
 
-prices = pd.read_csv('data/spy_2000-2020.csv', index_col='Date', parse_dates=True)
+#prices = pd.read_csv('data/spy_2000-2020.csv', index_col='Date', parse_dates=True)
+prices = pd.read_csv('data/spy_5yrs.csv', index_col='Date', parse_dates=True)
+
 
 # initialize the Cerebro engine
 cerebro = Cerebro()
@@ -27,10 +29,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("strategy", help="Which strategy to run", type=str)
 args = parser.parse_args()
 
-if not args.strategy in strategies:
+
+if args.strategy == "golden_cross_opt":
+    strats = cerebro.optstrategy(
+        GoldenCross,
+        fast=range(40, 60, 10), slow=range(160, 220, 20),
+        #fast=range(10, 30, 10), slow=range(50, 90, 20),
+
+    )
+    cerebro.run(maxcpus=1)
+    GoldenCross.show_max()
+
+elif not args.strategy in strategies:
     print("Invalid strategy, must select one of {}".format(strategies.keys()))
     sys.exit()
 
-cerebro.addstrategy(strategy=strategies[args.strategy])
-cerebro.run()
-cerebro.plot()
+else:
+    cerebro.addstrategy(strategy=strategies[args.strategy])
+    cerebro.run()
+    cerebro.plot()
+
--- strategies/GoldenCross.py
+++ strategies/GoldenCross.py
@@ -1,10 +1,20 @@
-import os, math
-import sys
-import pandas as pd
+import math
 import backtrader as bt
 
+
 class GoldenCross(bt.Strategy):
+
+    results = []
+    #max_roi = 0
+    #max_fast = 0
+    #max_slow = 0
+
     params = (('fast', 50),
+              ('slow', 160),
+              ('order_pct', 0.95),
+              ('ticker', 'SPY'))
+
+    params_orig = (('fast', 50),
               ('slow', 200),
               ('order_pct', 0.95),
               ('ticker', 'SPY'))
@@ -26,6 +36,11 @@ class GoldenCross(bt.Strategy):
             self.fastma, 
             self.slowma
         )
+        self.size = 0
+        self.roi = 0
+
+    def start(self):
+            self.val_start = self.broker.get_cash()  # keep the starting cash
 
     def next(self):
         if self.position.size == 0:
@@ -33,10 +48,33 @@ class GoldenCross(bt.Strategy):
                 amount_to_invest = (self.p.order_pct * self.broker.cash)
                 self.size = math.floor(amount_to_invest / self.data.close)
 
-                print("Buy {} shares of {} at {}".format(self.size, self.p.ticker, self.data.close[0]))
+                #print("Buy {} shares of {} at {}".format(self.size, self.p.ticker, self.data.close[0]))
                 self.buy(size=self.size)
             
         if self.position.size > 0:
-            if (self.crossover < 0):
-                print("Sell {} shares of {} at {}".format(self.size, self.p.ticker, self.data.close[0]))
+            if self.crossover < 0:
+                #print("Sell {} shares of {} at {}".format(self.size, self.p.ticker, self.data.close[0]))
                 self.close()
+
+    def stop(self):
+        # calculate the actual returns
+        self.roi = (self.broker.get_value() / self.val_start) - 1.0
+        print('{}-{}:'.format(self.params.fast, self.params.slow), end="")
+
+        print(' ROI:        {:.2f}%'.format(100.0 * self.roi))
+
+        GoldenCross.results.append((self.params.fast, self.params.slow, round(100.0 * self.roi, 2)))
+        """
+        if self.roi > GoldenCross.max_roi:
+            GoldenCross.max_roi = self.roi
+            GoldenCross.max_fast = self.params.fast
+            GoldenCross.max_slow = self.params.slow
+        """
+
+    @classmethod
+    def show_max(cls):
+        #print("BEST: {}-{} : {:.2f}%".format(cls.max_fast, cls.max_slow, 100.0 * cls.max_roi)
+        #print(sorted(cls.max_res, key=cls.max_res[]))
+        cls.results.sort(key=lambda x: x[2], reverse=True)
+        for result in cls.results:
+            print(result)
